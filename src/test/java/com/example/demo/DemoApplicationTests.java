4425f3a061deb3463e4c8e450dcb79f3180d290f
zeineb <zeinebsoltani8@gmail.com>
2025-04-18 01:22:09 +0200

    modification code2

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,4 +1,5 @@
 package com.example.demo;
+import static org.junit.jupiter.api.Assertions.assertEquals;  // JUnit 5
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
